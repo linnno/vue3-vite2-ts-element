82bf80dec9eb551578dd81bf2b514839a4ef6256
wangxi <1400785619@qq.com>
2021-06-22 10:00:34 +0800

    配置github

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,23 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vue3-vite2-ts-element/' : '/',
+  server: {
+    https: false,
+    // proxy: createProxy(VITE_PROXY),
+  },
   plugins: [vue()]
 })
+
+
+// export default({ command }) => {
+//   return {
+//     base: process.env.NODE_ENV === 'production' ? '/[项目名]/' : '/',
+//     // 服务端渲染
+//     server: {
+//       https: false,
+//       // proxy: createProxy(VITE_PROXY),
+//     },
+//     plugins: [vue()]
+//   };
+// };
