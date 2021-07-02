b284fd3e5740f5e22e686c26bda835ec28f808bf
wangxi <1400785619@qq.com>
2021-07-02 16:25:26 +0800

    添加element-plus和公共样式

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-
 import store from './store/index';
+
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import '@/styles/index.scss';
+
 const app = createApp(App);
 
 store.forEach(({ modelName, key }) => {
   app.use(modelName, key);
 });
 
+app.use(ElementPlus);
 app.use(router);
 app.mount('#app');
